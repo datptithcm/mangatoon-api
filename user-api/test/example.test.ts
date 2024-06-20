8e3a90cbbb53a96708b38b6dc3729e1211a643db
Nguyen Minh Dat <nguyenminhdat@MacBook-Air-cua-Nguyen.local>
2024-06-20 08:43:21 +0700

    updated

--- user-api/test/example.test.ts
+++ user-api/test/example.test.ts
@@ -3,9 +3,9 @@ function add(a: number, b: number): number {
 }
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(5);
+    expect(add(1, 2)).toBe(3);
 });
 
 test('adds 2 + 2 to equal 4', () => {
-    expect(add(2, 2)).toBe(3);
+    expect(add(2, 2)).toBe(4);
 });
